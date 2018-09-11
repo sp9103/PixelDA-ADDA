0dc572a408ab41e3bd04676b2e8974764835e8c8
Moons <sp9103@naver.com>
2018-09-11 13:23:56 +0900

    optimizer 변경, 학습 코드 추가

--- ADDA/train.py
+++ ADDA/train.py
@@ -1,14 +1,16 @@
+import logging
 import os
 import sys
 sys.path.append('../')
-#os.chdir('..') # 작업 위치 변경
 
 import tensorflow as tf
 import numpy as np
+from collections import deque
 from tqdm import tqdm
 from data_factory import dataset_factory
 import Preprocessing
 import Classifier
+import util
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -38,8 +40,6 @@ flags.DEFINE_integer(
     'The number of parallel readers that read data from the dataset.')
 
 flags.DEFINE_integer('iteration', 20000, '')
-
-flags.DEFINE_boolean('pre_training', True, '')
 
 flags.DEFINE_float('lr', 1e-4, '')
 
@@ -89,8 +89,9 @@ def main(_):
     class_loss = tf.losses.sparse_softmax_cross_entropy(source_labels['class'], net)
     loss = tf.losses.get_total_loss()
 
-    lr_var = tf.Variable(FLAGS.lr, name='learning_rate', trainable=False)
-    optimizer = tf.train.AdamOptimizer(lr_var)
+    lr = FLAGS.lr
+    lr_var = tf.Variable(lr, name='learning_rate', trainable=False)
+    optimizer = tf.train.MomentumOptimizer(lr_var, 0.99)
     step = optimizer.minimize(loss)
 
     config = tf.ConfigProto(device_count=dict(GPU=1))
@@ -100,8 +101,35 @@ def main(_):
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     sess.run(tf.global_variables_initializer())
 
-    if weights:
+    model_vars = util.collect_vars('source')
+    saver = tf.train.Saver(var_list=model_vars)
+    output_dir = os.path.join('snapshot', 'LeNet_mnist')
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
+    losses = deque(maxlen=10)
+    bar = tqdm(range(FLAGS.iteration))
+    bar.set_description('{} (lr: {:.0e})'.format('LeNet_mnist', lr))
+    bar.refresh()
+
+    display = 10
+    stepsize = None
+    for i in bar:
+        loss_val, _ = sess.run([loss, step])
+        losses.append(loss_val)
+        if i % display == 0:
+            logging.info('{:20} {:10.4f}     (avg: {:10.4f})'
+                        .format('Iteration {}:'.format(i),
+                                loss_val,
+                                np.mean(losses)))
+        if stepsize is not None and (i + 1) % stepsize == 0:
+            lr = sess.run(lr_var.assign(lr * 0.1))
+            logging.info('Changed learning rate to {:.0e}'.format(lr))
+            bar.set_description('{} (lr: {:.0e})'.format('LeNet_mnist', lr))
+        if (i + 1) % snapshot == 0:
+            snapshot_path = saver.save(sess, os.path.join(output_dir, 'LeNet_mnist'),
+                                       global_step=i + 1)
+            logging.info('Saved snapshot to {}'.format(snapshot_path))
 
     coord.request_stop()
     coord.join(threads)
---
+++ ADDA/util.py
@@ -0,0 +1,29 @@
+import os.path
+from collections import OrderedDict
+
+import tensorflow as tf
+
+def remove_first_scope(name):
+    return '/'.join(name.split('/')[1:])
+
+def collect_vars(scope, start=None, end=None, prepend_scope=None):
+    vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
+    var_dict = OrderedDict()
+    if isinstance(start, str):
+        for i, var in enumerate(vars):
+            var_name = remove_first_scope(var.op.name)
+            if var_name.startswith(start):
+                start = i
+                break
+    if isinstance(end, str):
+        for i, var in enumerate(vars):
+            var_name = remove_first_scope(var.op.name)
+            if var_name.startswith(end):
+                end = i
+                break
+    for var in vars[start:end]:
+        var_name = remove_first_scope(var.op.name)
+        if prepend_scope is not None:
+            var_name = os.path.join(prepend_scope, var_name)
+        var_dict[var_name] = var
+    return var_dict
