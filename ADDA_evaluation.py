52f180574685fe9f616716a36df8b3ac2bfe6901
Moons <sp9103@naver.com>
2018-09-13 17:18:50 +0900

    싱글 쓰레드로 변경

--- ADDA_evaluation.py
+++ ADDA_evaluation.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from data_factory import dataset_factory
 from ADDA import Classifier
 from ADDA import util
+import matplotlib.pyplot as plt
 
 slim = tf.contrib.slim
 
@@ -28,17 +29,17 @@ flags.DEFINE_string('target_split_name', 'train',
 flags.DEFINE_string('dataset_dir', './dataset',
                     'The directory where the datasets can be found.')
 
-flags.DEFINE_integer('num_preprocessing_threads', 4,
+flags.DEFINE_integer('num_preprocessing_threads', 1,
                      'The number of threads used to create the batches.')
 
 flags.DEFINE_integer(
-    'num_readers', 4,
+    'num_readers', 1,
     'The number of parallel readers that read data from the dataset.')
 
 def format_array(arr):
     return '  '.join(['{:.3f}'.format(x) for x in arr])
 
-def evalutation(session, net, label_batch, num_class, path, name, totalCount):
+def evalutation(session, net, label_batch, num_class, path, name, totalCount, InputImg = None):
     var_dict = util.collect_vars('source_only')
     restorer = tf.train.Saver(var_list=var_dict)
     output_dir = os.path.join(path, name)
@@ -54,11 +55,17 @@ def evalutation(session, net, label_batch, num_class, path, name, totalCount):
     class_counts = np.zeros(num_class, dtype=np.int32)
 
     with slim.queues.QueueRunners(session):
-        """
-        
-        """
+        plt.figure()
+        for i in range(16):
+            np_image = session.run(InputImg)
+            _, height, width, _ = np_image.shape
+            plt.subplot(4, 4, i + 1)
+            plt.imshow(np_image[0])
+            plt.title('%d x %d' % (height, width))
+            plt.axis('off')
+        plt.show()
 
-        for i in tqdm(range(totalCount)):
+        for i in tqdm(range(totalCount * 2)):
             predictions, gt = session.run([net, label_batch])
             class_counts[gt[0]] += 1
             if predictions[0] == gt[0]:
@@ -101,13 +108,26 @@ def main(_):
     net = tf.argmax(net, -1)
 
     sess.run(tf.global_variables_initializer())
+
+    #######################################evaluate source only####################################
     evalutation(sess,
                 net,
                 target_label,
                 num_target_classes,
                 'ADDA/snapshot',
                 'LeNet_mnist',
-                target_dataset.num_samples)
+                target_dataset.num_samples,
+                target_images)
+
+    #######################################evaluate adda############################################
+    evalutation(sess,
+                net,
+                target_label,
+                num_target_classes,
+                'ADDA/snapshot',
+                'LeNet_mnist',
+                target_dataset.num_samples,
+                target_images)
 
 if __name__ == '__main__':
     tf.app.run()
